d005c4e71871a7f1b4216ea840d33082ee6d0c74
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-03-23 17:59:03 -0300

    feat: create all get user

--- internal/repository/select.go
+++ internal/repository/select.go
@@ -1,18 +1,90 @@
 package repository
 
-import "github.com/ViitoJooj/clown-crm/internal/domain"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/ViitoJooj/clown-crm/internal/domain"
+)
 
 func (r *PostgresUserRepository) ListUsers() ([]*domain.User, error) {
+	rows, err := r.db.Query(context.Background(), `SELECT uuid, first_name, last_name, email, updated_at, created_at FROM users`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-	return nil, nil
+	var users []*domain.User
+
+	for rows.Next() {
+		var user domain.User
+
+		err := rows.Scan(
+			&user.UUID,
+			&user.First_Name,
+			&user.Last_Name,
+			&user.Email,
+			&user.Updated_at,
+			&user.Created_at,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
 
-func (r *PostgresUserRepository) FindUserByID(id string) (*domain.User, error) {
+func (r *PostgresUserRepository) FindUserByID(uuid string) (*domain.User, error) {
+	var user domain.User
 
-	return nil, nil
+	row := r.db.QueryRow(context.Background(), `SELECT uuid, first_name, last_name, email, updated_at, created_at FROM users WHERE uuid = $1`, uuid)
+
+	err := row.Scan(
+		&user.UUID,
+		&user.First_Name,
+		&user.Last_Name,
+		&user.Email,
+		&user.Updated_at,
+		&user.Created_at,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (r *PostgresUserRepository) FindUserByEmail(email string) (*domain.User, error) {
+	var user domain.User
 
-	return nil, nil
+	row := r.db.QueryRow(context.Background(), `SELECT uuid, first_name, last_name, email, updated_at, created_at FROM users WHERE email = $1`, email)
+
+	err := row.Scan(
+		&user.UUID,
+		&user.First_Name,
+		&user.Last_Name,
+		&user.Email,
+		&user.Updated_at,
+		&user.Created_at,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
