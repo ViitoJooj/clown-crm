c6db5d12dd3a8c8ae012808953a66fa281f916bd
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-03-23 17:11:39 -0300

    feat: create insert user

--- internal/repository/insert.go
+++ internal/repository/insert.go
@@ -1,8 +1,18 @@
 package repository
 
-import "github.com/ViitoJooj/clown-crm/internal/domain"
+import (
+	"context"
+	"errors"
 
-func (r *InMemoryUserRepository) CreateUser(user *domain.User) error {
+	"github.com/ViitoJooj/clown-crm/internal/domain"
+)
 
+func (r *PostgresUserRepository) CreateUser(user *domain.User) error {
+	err := r.db.QueryRow(context.Background(), "INSERT INTO users (uuid, first_name, last_name, email, password, updated_at, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+		user.UUID, user.First_Name, user.Last_Name, user.Email, user.Password, user.Updated_at, user.Created_at,
+	)
+	if err != nil {
+		return errors.New("internal error")
+	}
 	return nil
 }
