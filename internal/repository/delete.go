8f3910a071707ff2bd9da81f51b14a7d2e2daaed
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-03-24 09:02:12 -0300

    feat: add repository methods

--- internal/repository/delete.go
+++ internal/repository/delete.go
@@ -1,6 +1,23 @@
 package repository
 
+import (
+	"context"
+	"errors"
+)
+
 func (r *PostgresUserRepository) DeleteUserById(id string) error {
+	res, err := r.db.Exec(
+		context.Background(),
+		"DELETE FROM users WHERE uuid = $1",
+		id,
+	)
+	if err != nil {
+		return errors.New("internal error")
+	}
+
+	if res.RowsAffected() == 0 {
+		return errors.New("user not found")
+	}
 
 	return nil
 }
--- internal/repository/update.go
+++ internal/repository/update.go
@@ -1,8 +1,33 @@
 package repository
 
-import "github.com/ViitoJooj/clown-crm/internal/domain"
+import (
+	"context"
+	"errors"
+
+	"github.com/ViitoJooj/clown-crm/internal/domain"
+)
 
 func (r *PostgresUserRepository) UpdateUser(user *domain.User) error {
+	_, err := r.db.Exec(
+		context.Background(),
+		`UPDATE users 
+		 SET first_name = $1,
+		     last_name = $2,
+		     email = $3,
+		     password = $4,
+		     updated_at = $5
+		 WHERE uuid = $6`,
+		user.First_Name,
+		user.Last_Name,
+		user.Email,
+		user.Password,
+		user.Updated_at,
+		user.UUID,
+	)
+
+	if err != nil {
+		return errors.New("internal error")
+	}
 
 	return nil
 }
